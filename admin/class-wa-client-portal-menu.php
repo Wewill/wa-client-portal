bfcb8da2ccba8d116191fa6836f34ccd0be2ef86
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2025-07-23 09:48:32 +0200

    fix: Add rights to add_menu_page role editor

--- admin/class-wa-client-portal-menu.php
+++ admin/class-wa-client-portal-menu.php
@@ -6,7 +6,7 @@ function wa_client_portal_admin_menu() {
 	add_menu_page(
 		__('Members', 'wacp'),
 		__('Members', 'wacp'),
-		'list_users',
+		'add_menu_page',
 		'wa-client-portal-clients',
 		'wa_client_portal_clients_page',
 		'dashicons-editor-table',
