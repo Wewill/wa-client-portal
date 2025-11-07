a769928b9e5e4a3aff3ca159acbabc35dcff5aea
Wilhem Arnoldy <contact@wilhemarnoldy.fr>
2025-11-07 21:47:44 +0100

    Fix page title subtitle before hook

--- public/class-wa-client-portal-pagetitle.php
+++ public/class-wa-client-portal-pagetitle.php
@@ -16,9 +16,13 @@ add_filter( 'private_title_format', function ( $format ) {
 
 // Add a small h6 title before the title of private pages
 add_filter('the_title', function($title, $post_id) {
-	$post = get_post($post_id);
-	if ($post && $post->post_status === 'private' && !is_admin()) {
-		$title = '<h6 class="private-page headline">' . __('My account', 'wacp') . '</h6>' . $title;
-	}
-	return $title;
-}, 10, 2);
+    if (!is_admin() && is_singular() && in_the_loop() && get_post_status($post_id) === 'private') {
+        // Output only once, before main title
+        static $printed = false;
+        if (!$printed) {
+            echo '<h6 class="private-page headflat fs-2 d-inline mb-0">' . __('My account', 'wacp') . '</h6>';
+            $printed = true;
+        }
+    }
+    return $title;
+}, 9, 2);
--- wa-client-portal.php
+++ wa-client-portal.php
@@ -16,7 +16,7 @@
  * Plugin Name:       WA Private Client Portal
  * Plugin URI:        https://www.wilhemarnoldy.fr
  * Description:       WordPress Client Portal Plugin — fifam version — that creates private pages for all users that only an administrator can edit.
- * Version:           1.2.0
+ * Version:           1.3.0
  * Author:            Wilhem Arnoldy
  * Author URI:        https://www.wilhemarnoldy.fr/
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'WA_CLIENT_PORTAL_VERSION', '1.2.0' );
+define( 'WA_CLIENT_PORTAL_VERSION', '1.3.0' );
 
 /**
  * The code that runs during plugin activation.
